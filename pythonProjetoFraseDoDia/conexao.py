75f57ad6dfea32f1a5ac263934140c9a0916ccd3
matheusbarros2002 <barrosmatheusvieira@hotmail.com>
2022-06-10 14:54:24 -0300

    teste

--- pythonProjetoFraseDoDia/conexao.py
+++ pythonProjetoFraseDoDia/conexao.py
@@ -1,12 +1,12 @@
-#conexão com banco de dados
+# conexão com banco de dados
 
 from pymongo import MongoClient
 
-client= MongoClient("mongodb://localhost:27017/")
+client = MongoClient("mongodb://localhost:27017/")
 
-#print(client.list_database_names())
+# print(client.list_database_names())
 
 meu_banco = client['famosos']
 colecao = meu_banco['cadastro']
-for item in colecao.find({'_id':0}, {'nome':1}):
+for item in colecao.find({'_id': 1}, {'nome': 1}):
     print(item)
